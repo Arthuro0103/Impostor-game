92597660082c5f280805972ff6e932c99de0272f
arthur <arthurbparis2@gmail.com>
2024-09-07 21:11:40 -0300

    js-debug-2

---
+++ Impostor/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+
+const PORT =2000;
+app.listen({
+port: PORT,
+host: "0.0.0.0"
+});
